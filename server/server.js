3a3aede8649cd9041ff8469fdd9238410e4594ee
Tara Grimm <tararochegrimm@gmail.com>
2017-08-02 16:43:47 -0400

    added comments to clarify what is occuring in the app

--- server/server.js
+++ server/server.js
@@ -7,15 +7,20 @@ let { User } = require("./models/user");
 
 let app = express();
 
-app.use(bodyParser.json());
+//configure middleware (using bodyParser to allow us to send JSON to our express app)
+app.use(bodyParser.json()); 
 
 app.post("/todos", (req, res) => {
+    
+    //create To Do document from the request body (uses Mongoose model from other 
     let todo = new Todo({
         text: req.body.text,
         completed: req.body.completed,
         completedAt: req.body.completedAt
+
     });
 
+    //save model to database
     todo.save().then((doc) => {
         res.send(doc);
     }, (err) => {
