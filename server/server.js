0885192c3a9d199c0fe7b697e583ab81c9ca1e54
Tara Grimm <tararochegrimm@gmail.com>
2017-08-02 19:41:32 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ let { User } = require("./models/user");
 let app = express();
 
 //configure middleware (using bodyParser to allow us to send JSON to our express app)
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 app.post("/todos", (req, res) => {
 
@@ -28,8 +28,17 @@ app.post("/todos", (req, res) => {
     });
 });
 
+//fetches all the documents in the collection
+app.get("/todos", (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos }); //better to send back an object instead of array so you can other properties in later such as custom status codes, etc.  Also, this is using es6 way of doing things, so instead of writing todos = todos can just write todos.
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(3000, () => {
     console.log("Started on port 3000");
 });
 
-module.exports = {app};
+module.exports = { app };
