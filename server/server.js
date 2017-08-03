e185e63ff092a5d2770ddbb54a5c2e9c77a98d3d
Tara Grimm <tararochegrimm@gmail.com>
2017-08-03 16:46:20 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 let { mongoose } = require("./db/mongoose");
 let { Todo } = require("./models/todo");
@@ -36,6 +37,27 @@ app.get("/todos", (req, res) => {
         res.status(400).send(e);
     });
 });
+
+// CHALLENGE
+app.get("/todos/:id", (req, res) => {
+    let id = req.params.id;
+
+    //validate the id - send back empy block
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send(); //return this to stop rest of function from going ahead
+    };
+
+    //use findById to find the ID and send back error statuses if needed
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+    }, (err) => {
+        res.status(400).send();
+    })
+})
+
 
 app.listen(3000, () => {
     console.log("Started on port 3000");
